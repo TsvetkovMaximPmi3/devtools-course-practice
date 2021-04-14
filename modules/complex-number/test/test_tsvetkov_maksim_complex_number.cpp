696d18a22f301adfc6b0d59820c4897f6dc88888
max <55163491+TsvetkovMaximPmi3@users.noreply.github.com>
2021-04-14 20:22:35 +0300

    Update test_tsvetkov_maksim_complex_number.cpp

--- modules/complex-number/test/test_tsvetkov_maksim_complex_number.cpp
+++ modules/complex-number/test/test_tsvetkov_maksim_complex_number.cpp
@@ -8,13 +8,12 @@
 TEST(Tsvetkov_Maksim_ComplexNumberTest, Pow_of_imaginary_unit) {
     // Arrange
     ComplexNumber z(0, 2.0);
-    ComplexNumber res(0,-8.0);
+    ComplexNumber res(0, -8.0);
     ComplexNumber res1(z);
 
     // Act
-    
-    for( int i = 0; i < 2; i++ )
-       res1=res1*z;
+    for ( int i = 0; i < 2; i++ )
+       res1 = res1*z;
     // Assert
     ASSERT_EQ(res, res1);
 }
