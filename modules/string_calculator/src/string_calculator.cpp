8d51467d004d2d99563dd0ca49f11446a1264b15
Maxim <none>
2021-05-27 01:10:23 +0300

    add new operations

--- modules/string_calculator/src/string_calculator.cpp
+++ modules/string_calculator/src/string_calculator.cpp
@@ -7,58 +7,20 @@
 #include <string>
 #include "include/string_calculator.h"
 
-StringCalculator::StringCalculator(const std::string& str):
-                                    my_string(str) {}
 
-bool isNumber(char a) {
+bool StringCalculator::isNumber(char a) {
     return (a < '9' && a > '0');
 }
 
-int StringCalculator::Add(std::string numbers) {
-    if (numbers == "") {
-        return 0;
-    }
-    int result = 0;
-
-    char separator = ',';
-
-    if (!isNumber(numbers[0])) {
-        separator = numbers[0];
-        numbers.erase(0, 2);
-    }
-
-    for (size_t i = 0; i < numbers.size(); ++i) {
-        if (numbers[i] == separator || numbers[i] == '\n') {
-            numbers[i] = ' ';
-        }
-    }
-    std::istringstream is(numbers);
-    std::string num;
-    std::vector<int> negatives;
-    while (is >> num) {
-        try {
-            int a = std::stoi(num);
-            if (a < 0) {
-                negatives.push_back(a);
-            } else {
-                result += a;
-            }
-        } catch(...) {
-            throw std::runtime_error("ERROR: Wrong number format!\n");
-        }
-    }
-    if (negatives.size() > 0) {
-        std::string error = "ERROR: negatives not allowed : ";
-        for (size_t i = 0; i < negatives.size(); ++i) {
-            error += std::to_string(negatives[i]);
-            error += " ";
-        }
-        error += "\n";
-        throw std::runtime_error(error);
-    }
-    return result;
+int StringCalculator::add(const std::string& str) {
+    return operation(str, std::plus<int>());
 }
 
-int StringCalculator::getResult() {
-    return Add(my_string);
+int StringCalculator::subtract(const std::string& str) {
+    return operation(str, std::minus<int>());
 }
+
+int StringCalculator::multiplicate(const std::string& str) {
+    return operation(str, std::multiplies<int>());
+}
+
--- modules/string_calculator/src/string_calculator_app.cpp
+++ modules/string_calculator/src/string_calculator_app.cpp
@@ -13,21 +13,28 @@ std::string StringCalculatorApp::help(const std::string& filename) {
     return "This is an simple String calculator application. " \
         "It return sum of given arguments. \n\n" \
         "Please provide arguments in the following format:\n$ " +
-        filename + " \"[delimiter]\\n[numbers...]\"\n" \
-        "Example: " + filename + " ;\\n1;2 \n\n";
+        filename + " [operation from {+,-,*}]" +
+        " \"[delimiter]\\n[numbers...]\"\n" \
+        "Example: " + filename + " + \";\\n1;2\" \n\n";
 }
 
 std::string StringCalculatorApp::operator()(int argc, const char** argv) {
-    if (argc == 1) {
+    if (argc != 3) {
         return help(argv[0]);
     }
 
     std::ostringstream ss;
     int res;
-    StringCalculator calc(argv[1]);
     try {
-        res = StringCalculator::Add(argv[1]);
-        assert(res == calc.getResult());
+        if (argv[1][0] == '+') {
+            res = StringCalculator::add(argv[2]);
+        } else {
+            if (argv[1][0] == '-') {
+                res = StringCalculator::subtract(argv[2]);
+            } else {
+                res = StringCalculator::multiplicate(argv[2]);
+            }
+        }
     } catch (std::exception& exc) {
         return exc.what() + help(argv[0]);
     }
